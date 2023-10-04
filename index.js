63c9cdb9b6f84149471456a10b643b3debfb6c8e
Raj kumar <124882286+rajkumar3918@users.noreply.github.com>
2023-10-04 15:25:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 app.use(json());
-app.use(urlencoded({extended:true}));
+app.use(urlencoded({extended:false}));
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
 
